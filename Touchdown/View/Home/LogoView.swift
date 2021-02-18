0835afa0c78ca548d20ed05d5361e331bfef29e4
Itunu Raimi <chavitunu@gmail.com>
2021-02-18 11:53:26 +0100

    designed navigationbar view

---
+++ Touchdown/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Itunu Raimi on 18/02/2021.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4, content: {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }) //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
