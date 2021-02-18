413dd64013b87ac9ebe6ef462a2f37692ebacea9
Itunu Raimi <chavitunu@gmail.com>
2021-02-18 09:20:55 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Itunu Raimi on 18/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
